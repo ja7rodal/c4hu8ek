8213518f58f35c6d3f83d8d6dce2ac997bb96720
Jasson Rdz <ja7rodal@gmail.com>
2017-01-16 14:41:04 -0500

    authenticate tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	
+	def authenticate
+		user = User.find_by email: request.headers['X-User-Email'];
+		
+		if user_api_token != request.headers['X-Api-Token']
+			render json: { error: "Denied" }, status: 401			
+		else
+			return true
+		end
+		
+	end
+	
 end
